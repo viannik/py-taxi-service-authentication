eca54d7cb4d3929cedc6320f5ddf9a6d5f41e958
Dmytro Svirsa <104418771+DmytroSvirsa@users.noreply.github.com>
2022-08-17 15:16:34 +0300

    tests auth and more

---
+++ tests/test_taxi_service_forms.py
@@ -0,0 +1,112 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from taxi.models import Car, Manufacturer, Driver
+
+TestCase.fixtures = ['tests/taxi_service_db_data.json', ]
+
+
+class PublicTests(TestCase):
+    def test_car_list_login_required(self):
+        response = self.client.get(reverse("taxi:car_list"))
+
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_manufacturer_list_login_required(self):
+        response = self.client.get(reverse("taxi:manufacturer_list"))
+
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_driver_login_required(self):
+        response = self.client.get(reverse("taxi:driver_list"))
+
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_index_login_required(self):
+        response = self.client.get(reverse("taxi:index"))
+
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_login(self):
+        response = self.client.get(reverse("login"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/login.html")
+
+
+class PrivateHomeTests(TestCase):
+    def setUp(self) -> None:
+        self.client.login(username="admin.user", password="1qazcde3")
+
+    def test_index(self):
+        response = self.client.get(reverse("taxi:index"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "taxi/index.html")
+
+    def test_visit_counter(self):
+        visits = 3
+
+        for visit in range(visits):
+            response = self.client.get(reverse("taxi:index"))
+            self.assertEqual(response.context["num_visits"], visit + 1)
+
+
+class PrivateManufacturerTests(TestCase):
+    def setUp(self) -> None:
+        self.client.login(username="admin.user", password="1qazcde3")
+
+    def test_manufacturer_list(self):
+        response = self.client.get(reverse("taxi:manufacturer_list"))
+        manufacturers = Manufacturer.objects.all()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            list(response.context["manufacturer_list"]),
+            list(manufacturers[0:len(list(response.context["manufacturer_list"]))])
+        )
+        self.assertTemplateUsed(response, "taxi/manufacturer_list.html")
+
+
+class PrivateCarTests(TestCase):
+    def setUp(self) -> None:
+        self.client.login(username="admin.user", password="1qazcde3")
+
+    def test_car_list(self):
+        response = self.client.get(reverse("taxi:car_list"))
+        cars = Car.objects.all()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            list(response.context["car_list"]),
+            list(cars[0:len(list(response.context["car_list"]))])
+        )
+        self.assertTemplateUsed(response, "taxi/car_list.html")
+
+    def test_car_detail(self):
+        response = self.client.get(reverse("taxi:car_detail", args=[1]))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "taxi/car_detail.html")
+
+
+class PrivateDriverTests(TestCase):
+    def setUp(self) -> None:
+        self.client.login(username="admin.user", password="1qazcde3")
+
+    def test_driver_list(self):
+        response = self.client.get(reverse("taxi:driver_list"))
+        drivers = Driver.objects.all()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            list(response.context["driver_list"]),
+            list(drivers[0:len(list(response.context["driver_list"]))])
+        )
+        self.assertTemplateUsed(response, "taxi/driver_list.html")
+
+    def test_driver_detail(self):
+        response = self.client.get(reverse("taxi:driver_detail", args=[1]))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "taxi/driver_detail.html")
